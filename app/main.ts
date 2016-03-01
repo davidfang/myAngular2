d57c5ed25653306eba240d138671d54fb8e9967f
davidfang <mw_fang@163.com>
2016-03-01 19:49:27 +0800

    Milestone #2之前

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
-/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
-import {bootstrap}    from 'angular2/platform/browser'
-import {AppComponent} from './app.component'
-bootstrap(AppComponent);
+import {AppComponent}     from './app.component';
+import {bootstrap}        from 'angular2/platform/browser';
+import {ROUTER_PROVIDERS} from 'angular2/router';
+bootstrap(AppComponent, [
+    ROUTER_PROVIDERS
+]);
